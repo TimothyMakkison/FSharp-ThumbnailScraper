2c91d78fba30118450637d10c76799d8e5236d9f
Timothy Makkison <timmakkison@gmail.com>
2022-01-04 22:45:37 +0000

    Add argument parsing with Argu

--- ThumbnailScraper/Program.fs
+++ ThumbnailScraper/Program.fs
@@ -1,21 +1,34 @@
-﻿open Google.Apis.Services
+﻿open System
+open Google.Apis.Services
 open System.IO
 open Google.Apis.YouTube.v3
-open System.Threading.Tasks
 open System.Net.Http
 open System.Text.RegularExpressions
-open System
+open FSharp.Control
+open Argu
 
 let apiKey = File.ReadAllText("./google-api-key.txt")
 let saveLocation = @"C:\Users\timma\Desktop\ImageData\ColorThumbnails\";
 
-type VideoData = {Title:string; Url:string}
+type CommandArguments = 
+        |[<Mandatory>] Save_Path of path:string
+        |[<Mandatory>] Channel_Name of channel:string
+        | After_Date of from_date:string 
+
+        interface IArgParserTemplate with
+              member s.Usage =
+                  match s with
+                  | Save_Path _ -> "specify the save location."
+                  | Channel_Name _ -> "specify the youtube channel to scrape."
+                  | After_Date _ -> "specify the date from which uploads will be collected."
+
+type VideoData = {Title:string; Url:string; Upload: DateTime option}
 
 let getFileExtension url = 
     let uri = url  |> Uri
     uri.GetLeftPart(UriPartial.Path) |> Path.GetExtension
 
-let getPath folder title url = 
+let createPath folder title url = 
     let fileName = Regex.Replace(title, @"[\\/:*?""<>|]", "")
     let fileExtension = getFileExtension url
 
@@ -23,98 +36,212 @@ let getPath folder title url =
 
 let downloadImage title url folder (client:HttpClient) = 
     async {
-        let path = getPath folder title url
+        let path = createPath folder title url
 
         let! imageBytes = client.GetByteArrayAsync(url) |> Async.AwaitTask
         return! File.WriteAllBytesAsync(path, imageBytes) |> Async.AwaitTask
     }
 
-let downloadAllImages images folderPath = 
-    task{
+let playlistItemToVideoData (item:Data.PlaylistItem ) =
+    let nullUpload = item.Snippet.PublishedAt
+    let upload = if nullUpload.HasValue then Some  nullUpload.Value else None
+    {Title=item.Snippet.Title; Url=item.Snippet.Thumbnails.Default__.Url; Upload= upload}
+
+let rec getVideoPage (service:YouTubeService) playlistId pageToken = 
+    async {
+        if pageToken = null then   
+            return None
+        else
+            let mutable request = service.PlaylistItems.List("snippet")
+            
+            request.PlaylistId <- playlistId
+            request.PageToken <- pageToken
+            request.MaxResults <- 50L
+
+            let! response = request.ExecuteAsync() |> Async.AwaitTask
+
+            printfn "Retrieved %d items from id: %s with token %s" response.Items.Count playlistId pageToken
+
+            let pageData = response.Items :> seq<_> 
+            let nextToken= response.NextPageToken
+
+            return Some (pageData, nextToken)
+            }
+
+let getAllVideoPages service playlistId = 
+    let rec loop token = asyncSeq {
+        let! result = getVideoPage service playlistId token 
+
+        match result with 
+        | Some (pages, nextToken) ->
+            for page in pages do
+                yield page
+
+            yield! loop nextToken
+        | _ -> ()
+    }
+
+    loop ""
+
+let downloadAllImages (images: AsyncSeq<VideoData>) folderPath = 
+    async {
         let client = new HttpClient()
         Directory.CreateDirectory(folderPath) |> ignore
 
-        return! images |> Seq.map (fun x -> downloadImage x.Title x.Url folderPath client  ) |> Async.Parallel |> Async.Ignore;
-    }
-    
-
-
-let playlistItemToVideoData (item:Data.PlaylistItem )= {Title=item.Snippet.Title; Url=item.Snippet.Thumbnails.Default__.Url}
-
-let getAllPages service playlistId = 
-
-    let rec getPage (service:YouTubeService) playlistId pageToken (data :seq<VideoData>) = 
-        task {
-            if pageToken = null then   
-                return data
-            else
-                let mutable request = service.PlaylistItems.List("snippet")
-
-                request.PlaylistId <- playlistId
-                request.PageToken <- pageToken
-                request.MaxResults <- 50L
-
-                let! response = request.ExecuteAsync() 
-
-                let pageData = response.Items :> seq<_> |> Seq.map playlistItemToVideoData
-
-                printfn "Retrieved %d items with page token %s." (pageData |> Seq.length) pageToken
-
-                let nextToken= response.NextPageToken
-                let pageData = pageData |> Seq.append data
-
-                return! getPage service playlistId nextToken pageData
+        do! images |> AsyncSeq.iterAsyncParallel (fun x -> printfn "%A" x; downloadImage x.Title x.Url folderPath client)
     }
 
-    getPage service playlistId "" Seq.empty
-
-let downloadImages channelName = task {
-
-    //Create service and get channel id
-    let baseClient = new BaseClientService.Initializer(ApiKey = apiKey, ApplicationName= "ThumbnailDownloader") 
-    let service = new YouTubeService(baseClient)
-
-    let mutable channelsListRequest:ChannelsResource.ListRequest  = service.Channels.List("contentDetails")
+let getChannelUploadsPlaylistId (service:YouTubeService) channelName = async{
+    let mutable channelsListRequest:ChannelsResource.ListRequest = service.Channels.List("contentDetails")
     channelsListRequest.ForUsername <- channelName;
 
-    let! response =  channelsListRequest.ExecuteAsync() 
+    let! response =  channelsListRequest.ExecuteAsync() |> Async.AwaitTask
 
     let channel = response.Items :> seq<_> |> Seq.head
     let playlistId = channel.ContentDetails.RelatedPlaylists.Uploads
-    printfn "Retrieved id: %s for channel: %s" playlistId channelName
-
-    let! pages = getAllPages service playlistId
-
-    // Print all pages
-    printfn "%A" (Seq.toList pages)
-    printfn "Retrieved %d items from the channel %s" (pages |> Seq.length) channelName
-    
-    return pages
+    return playlistId
 }
 
+let getChannelUploads channelName = asyncSeq {
+    //Create service and get channel id
+    let baseClient = new BaseClientService.Initializer(ApiKey = apiKey, ApplicationName = "ThumbnailDownloader") 
+    let service = new YouTubeService(baseClient)
+
+    let! playlistId = getChannelUploadsPlaylistId service channelName
+    printfn "Retrieved id: %s for channel: %s" playlistId channelName
+
+    //let! pages = getAllVideoPages service playlistId |> AsyncSeq.take 60 |> AsyncSeq.map playlistItemToVideoData |> AsyncSeq.iter (printfn "%A") 
+    //return pages
+
+    yield! getAllVideoPages service playlistId
+}
+
+let predi vid = 
+    match vid.Upload with
+    | Some value -> value > DateTime.Parse "12/1/2019 8:30:52 AM";
+    | _-> false
+
+let randomwait time=
+       async{
+           Console.WriteLine( $"started waiting :{time}")
+           do! Async.Sleep(time*1000);
+           Console.WriteLine( $"Waited:{time}")
+           return time
+       }
+
+let stream=
+    asyncSeq { 
+
+        for waitTime = 10 to 13 do
+            yield randomwait waitTime
+        for waitTime = 5 downto 3 do
+            yield randomwait waitTime
+        Console.WriteLine( $"Now printing second values")
+    }
+// Increment or decrement by one.
+type CounterMessage =
+    | Increment
+    | Decrement
+
+let createProcessor initialState =
+    MailboxProcessor<CounterMessage>.Start(fun inbox ->
+        // You can represent the processor's internal mutable state
+        // as an immutable parameter to the inner loop function
+        let rec innerLoop state = async {
+            printfn "Waiting for message, the current state is: %i" state
+            let! message = inbox.Receive()
+            // In each call you use the current state to produce a new
+            // value, which will be passed to the next call, so that
+            // next message sees only the new value as its local state
+            match message with
+            | Increment ->
+                let state' = state + 1
+                printfn "Counter incremented, the new state is: %i" state'
+                return! innerLoop state'
+            | Decrement ->
+                let state' = state - 1
+                printfn "Counter decremented, the new state is: %i" state'
+                return! innerLoop state'
+        }
+        // We pass the initialState to the first call to innerLoop
+        innerLoop initialState)
+
+// Let's pick an initial value and create the processor
+let processor = createProcessor 10
+
+open FSharp.Control.Reactive
+open System.Threading.Tasks
+open System.Threading
+
+let counter =
+    MailboxProcessor.Start(fun inbox ->
+        let rec loop n =
+            async { do printfn "n = %d, waiting..." n
+                    let! msg = inbox.Receive()
+                    return! loop(n+msg) }
+        loop 0);;
+
+let makeCountingTask (counter:MailboxProcessor<int>) taskId  = async {
+    let name = sprintf "Task%i" taskId
+    for i in [1..3] do
+        counter.Post(i)
+    }
+
 [<EntryPoint>]
-let main args =
-    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+let main argv =
+    Console.WriteLine("Printing")
+    let counter = counter
+    counter.Post(5) 
+    counter.Post(15)
 
-    //let channelName = "CloudKidOfficial"
+    let messageExample5 =
+        [1..5]
+            |> List.map (fun i -> makeCountingTask counter i)
+            |> Async.Parallel
+            |> Async.RunSynchronously
+            |> ignore
 
-    //let task = downloadImages channelName
-    //printfn "%f" stopWatch.Elapsed.TotalMilliseconds
-    let client = new HttpClient()
+    Thread.Sleep(2000);
+
+    //let task1 = stream |> AsyncSeq.mapAsyncParallel id // This runs all our randomWait tasks at once
+    //            |> AsyncSeq.mapAsyncParallel randomwait
+    //            |>AsyncSeq.iterAsyncParallel(fun time ->async{ Console.WriteLine($"printing for time : {time}")})
+    //Async.RunSynchronously task1
+
+    //Console.WriteLine("------Second point------")
+    //let task = stream |> AsyncSeq.toObservable |> Observable.bind Observable.ofAsync |> Observable.map randomwait |> Observable.iter (fun time -> printfn "printing for time : %i" time)
+
+    //Observable.wait task |> ignore
+
+    //let parser = ArgumentParser.Create<CommandArguments>()
+    //let results = parser.Parse(inputs = argv, raiseOnUsage = true) 
+    //let usage = parser.PrintUsage() 
+
+    //let channelName = results.GetResult Channel_Name
+    //let path = results.GetResult Save_Path
+
+    //printfn "%s" usage
+    //printfn "%s" path
+    //printfn "%s" channelName
+
+
+    //let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+
+    //    // Get id
+    //    // Get iterator for each video
+    //    // Takewhile true
+    //    // Download to path return data
+
+    //let downloader images = downloadAllImages images path 
     
-    let url = @"https://pbs.twimg.com/media/FHs5cUQXIAACYYI?format=jpg&name=large"
-    let path = @"C:\Users\timma\Desktop\coding\F#\ThumbnailScraper\ThumbnailScraper\"
-    downloadImage "The_Doc" url path client |> Async.RunSynchronously
+    //let a = getChannelUploads channelName |> AsyncSeq.map playlistItemToVideoData 
+    //        |> AsyncSeq.takeWhile predi 
+    //        |> downloader 
+
+    //a |> Async.RunSynchronously
+    //        //|> AsyncSeq.
+    //        //|> downloadAllImages
+
+    //printfn "%f" stopWatch.Elapsed.TotalMilliseconds
 
     0
-
-
-//Should 
-//Take file path, url and config
-//Check valid path
-
-//Go to url
-//Collect all videos
-//Foreach extract thumbnail
-
-//go to next page
